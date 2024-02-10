b684f706c05575217fbf223178bdd0e0232f1aac
CedricHermansBIT <cedric.hermans2@howest.be>
2024-02-10 19:35:11 +0100

    Refactor line splitting in diff.py for improved readability (and speed?)

--- diff.py
+++ diff.py
@@ -8,5 +8,6 @@ file2 = sys.argv[2]
 with open(file1, 'r') as f1, open(file2, 'r') as f2:
     for line1, line2 in zip(f1, f2):
         if line1 != line2:
+            spline1 = line1.split('\t')
             # split the lines on tab, print the first, fourth, sixth and last fields side by side for each file
-            print('\t'.join([line1.split('\t')[0], line1.split('\t')[3], line1.split('\t')[5], line1.split('\t')[-1].strip()]), '\t', line2.split('\t')[-1].strip())
+            print('\t'.join([spline1[0], spline1[2], spline1[3], spline1[5], spline1[-1].strip()]), '\t', line2.split('\t')[-1].strip())
