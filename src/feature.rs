a2f40b99cbd81752d8395a42aab0e23945ac951f
CedricHermansBIT <cedric.hermans2@howest.be>
2024-02-10 22:01:13 +0100

    Refactor Feature struct and add new methods

--- src/feature.rs
+++ src/feature.rs
@@ -1,9 +1,44 @@
+#![allow(dead_code)]
+
 // Struct to store the features
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Default)]
 pub struct Feature {
     //type_: String,
-    pub name: String,
-    pub chr: i32,
-    pub start: i32,
-    pub end: i32,
+    name: String,
+    chr: i32,
+    start: i32,
+    end: i32,
+    strand: char,
+}
+
+impl Feature {
+    pub fn new(name: String, chr: i32, start: i32, end: i32, strand: char) -> Self {
+        Feature {
+            name,
+            chr,
+            start,
+            end,
+            strand,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn chr(&self) -> i32 {
+        self.chr
+    }
+
+    pub fn start(&self) -> i32 {
+        self.start
+    }
+
+    pub fn end(&self) -> i32 {
+        self.end
+    }
+
+    pub fn strand(&self) -> char {
+        self.strand
+    }
 }
